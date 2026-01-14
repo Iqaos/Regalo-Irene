2be68dc6106bd9e0ce8f872a7f0cea9b39992cbe
Iqaos <eryk.tini@gmail.com>
2026-01-15 00:22:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let selectedCity = "";
+
+function goToPage(pageNumber) {
+  document.getElementById("page1").style.display = "none";
+  document.getElementById("page2").style.display = "none";
+  document.getElementById("page3").style.display = "none";
+
+  document.getElementById("page" + pageNumber).style.display = "block";
+}
+
+function selectCity(city) {
+  selectedCity = city;
+  alert("Hai scelto " + city + " 💖");
+}
 
