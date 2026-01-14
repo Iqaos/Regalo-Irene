111d4501cea141f75eb22f905b98e880801d577a
Iqaos <eryk.tini@gmail.com>
2026-01-15 00:31:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let currentPage = 1;
 let selectedCity = "";
 
 function goToPage(pageNumber) {
+  currentPage = pageNumber;
+
   document.getElementById("page1").style.display = "none";
   document.getElementById("page2").style.display = "none";
   document.getElementById("page3").style.display = "none";
@@ -8,8 +11,13 @@ function goToPage(pageNumber) {
   document.getElementById("page" + pageNumber).style.display = "block";
 }
 
+function goBack() {
+  if (currentPage > 1) {
+    goToPage(currentPage - 1);
+  }
+}
+
 function selectCity(city) {
   selectedCity = city;
   alert("Hai scelto " + city + " 💖");
 }
-
